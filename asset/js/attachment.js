808ecf04566341ed093c7c4ff06d98768d3336d4
DJ <donwalker1987@gmail.com>
2016-05-15 20:48:44 -0400

    add semanticui and other assets

---
+++ asset/js/attachment.js
@@ -0,0 +1,48 @@
+(function ($) {
+    var file_frame;
+    var $currentAttachmentField;
+
+    $(document).ready(function () {
+        $('#poststuff').on('click', '.ptconfig-attachment-upload', function (e) {
+            e.preventDefault();
+            $currentAttachmentField = $(this);
+
+            if (file_frame) {
+                file_frame.open();
+                return;
+            }
+
+            file_frame = wp.media.frames.file_frame = wp.media({
+                title: 'Add Media',
+                button: {
+                    text: 'Select'
+                },
+                multiple: false
+            });
+
+            file_frame.on('select', function () {
+                var attachment = file_frame.state().get('selection').first().toJSON();
+                processFileSelection(attachment);
+            });
+
+            file_frame.open();
+        });
+        $('#poststuff').on('click', '.ptconfig-attachment-remove', function () {
+            var $fieldContainer = $(this).parent('.ptconfig-form-field');
+            $fieldContainer.find('input').val('');
+            $fieldContainer.find('img').hide().attr('src', '');
+            $fieldContainer.find('.filename').text('').attr('href','').hide();
+            $(this).hide();
+        });
+    });
+    function processFileSelection(attachment) {
+        console.log(attachment);
+        var $fieldContainer = $currentAttachmentField.parent('.ptconfig-form-field');
+        $fieldContainer.find('input').val(attachment.url);
+        if (attachment.type === 'image') {
+            $fieldContainer.find('img').attr('src', attachment.url).show();
+        }
+        $fieldContainer.find('.filename').text(attachment.filename).attr('href',attachment.url).show();
+        $fieldContainer.find('.ptconfig-attachment-remove').show();
+    }
+})(jQuery);
