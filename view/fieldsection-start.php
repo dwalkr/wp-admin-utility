620dc2c957787360e72465df1748bd33493ec45e
DJ <donwalker1987@gmail.com>
2016-05-16 22:05:58 -0400

    styles

--- view/fieldsection-start.php
+++ view/fieldsection-start.php
@@ -1,2 +1,2 @@
-<fieldset class="ui raised segment">
-    <h2 class="ui header"><?=$this->configData->title;?></h2>
+<fieldset class="ui raised segment <?=property_exists($this->configData,'class') ? $this->configData->class : '';?>">
+    <h3 class="ui blue header"><?=$this->configData->title;?></h3>
--- view/tabs-nav.php
+++ view/tabs-nav.php
@@ -1,4 +1,5 @@
-<div class="ui menu top adminTabs">
+<div></div>
+<div class="ui compact menu top adminTabs">
     <?php foreach ($tabs as $i=>$tab) : ?>
     <a class="item<?=($i==0) ? ' active' : '';?>" data-tab="<?=sanitize_title_with_dashes($tab);?>"><?=esc_html($tab);?></a>
     <?php endforeach; ?>
