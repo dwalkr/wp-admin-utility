dc2335344ad48e1c1bd0d67e592ec4cf9c8c9ed1
DJ <donwalker1987@gmail.com>
2016-05-19 22:28:48 -0400

    sanitize HTML ids for brackets etc

--- view/field/date.php
+++ view/field/date.php
@@ -1,8 +1,9 @@
 <?php
 $format = property_exists($this->configData, 'format') ? $this->configData->format : 'yyyy-mm-dd';
+$eleId = sanitize_title_with_dashes($this->getKey());
 ?>
 
-<div class="ui calendar" id="uicalendar_<?=esc_attr($this->getKey());?>">
+<div class="ui calendar" id="uicalendar_<?=esc_attr($eleId);?>">
     <div class="ui input left icon">
         <i class="calendar icon"></i>
         <input type="text"
@@ -15,7 +16,7 @@ $format = property_exists($this->configData, 'format') ? $this->configData->form
 <script>
     (function($){
         $(document).ready(function(){
-           var $calendar = $('#uicalendar_<?=esc_attr($this->getKey());?>');
+           var $calendar = $('#uicalendar_<?=esc_attr($eleId);?>');
            $calendar.calendar({
                type: 'date',
                selector: {
